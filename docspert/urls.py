5b9f75c6951474b951990e73c232edb92677da23
Mohamed Elgamal <egmohamedelgamal@gmail.com>
2024-12-18 16:20:44 +0200

    Update urls.py

--- docspert/urls.py
+++ docspert/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from accounts.views import AccountsHomeView
 
 urlpatterns = [
-    path("", AccountsHomeView.as_view()),
+    path("", AccountsHomeView.as_view(), name="accounts_home"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
 ]
